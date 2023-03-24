3f7125340bb49940b0e77e2d1399a34f7524b41f
Eoin Quinn <equinn86@gmail.com>
2023-03-24 16:07:44 +0000

    Tile creation for terrains

--- Python/myFirstPlatformer/level.py
+++ Python/myFirstPlatformer/level.py
@@ -9,15 +9,35 @@ class Level:
     def __init__(self,level_data,surface):
         # level setup
         self.display_surface = surface
-        self.setup_level(level_data)
+        # self.setup_level(level_data)
         self.world_shift = 0
-        self.current_x = 0
+        # self.current_x = 0
 
+        # terrain setup
         terrain_layout = import_csv_layout(level_data['terrain'])
+        self.terrain_sprites = self.create_tile_group(terrain_layout,'terrain')
+
 
         # dust 
         self.dust_sprite = pygame.sprite.GroupSingle()
         self.player_on_ground = False
+
+    def create_tile_group(self,layout,type):
+        sprite_group = pygame.sprite.Group()
+
+        for row_index, row in enumerate(layout):
+            for col_index, val in enumerate(row):
+                if val != '-1':
+                    x = col_index * tile_size
+                    y = row_index * tile_size
+
+                    if type == 'terrain':
+                        sprite = Tile(tile_size,x,y)
+                        sprite_group.add(sprite)
+
+
+
+        return sprite_group
 
     def create_jump_particles(self,pos):
         if self.player.sprite.facing_right:
@@ -124,9 +144,13 @@ class Level:
         self.dust_sprite.draw(self.display_surface)
 
         # level tiles
-        self.tiles.update(self.world_shift)
-        self.tiles.draw(self.display_surface)
-        self.scroll_x()
+        self.terrain_sprites.draw(self.display_surface)
+        self.terrain_sprites.update(self.world_shift)
+    
+
+        # self.tiles.update(self.world_shift)
+        # self.tiles.draw(self.display_surface)
+        # self.scroll_x()
 
 
         # Player Sprite
