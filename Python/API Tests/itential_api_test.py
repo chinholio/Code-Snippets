ed3e892ead3da1313d51327832eada57eb65d6dc
Eoin Quinn <equinn86@gmail.com>
2023-03-25 17:55:42 +0000

    itential_test

---
+++ Python/API Tests/itential_api_test.py
@@ -0,0 +1,14 @@
+import json
+import requests
+
+def iten_login():
+    itenURL = "https://test.itential.com/"
+    username = "test"
+    password = "test"
+
+    headers = headers = {
+            'x-rapidapi-host': 'covid-19-statistics.p.rapidapi.com',
+            'x-rapidapi-key': '0510b8ed98mshc1dc586963910e7p104896jsn0255b9f45530'
+            }
+
+    itential_login = requests.post(itenURL,)
