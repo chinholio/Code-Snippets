866a72d1542bd3f6d2a4cd64ca53e71afd9189bd
chinholio <37453444+chinholio@users.noreply.github.com>
2024-04-15 13:47:13 +0100

    Update formSample.ps1

--- Powershell/formSample.ps1
+++ Powershell/formSample.ps1
@@ -2,10 +2,87 @@ function Show-MyPowerShellForm {
     param (
         [ref]$userInput,
         [ref]$selectedOption,
-        [ref]$selectedItem
+        [ref]$selectedItem,
+        [ref]$selectedFolder,
+        [ref]$checkBoxState
     )
 
-    # Rest of the form creation code (same as before)
+    # Load necessary .NET Framework classes
+    Add-Type -AssemblyName System.Windows.Forms
+    Add-Type -AssemblyName System.Drawing
+
+    # Create a new form
+    $form = New-Object System.Windows.Forms.Form
+    $form.Text = 'PowerShell GUI Example'
+    $form.Size = New-Object System.Drawing.Size(500, 300)
+    $form.StartPosition = 'CenterScreen'
+
+    # Add a label to instruct the user
+    $label = New-Object System.Windows.Forms.Label
+    $label.Location = New-Object System.Drawing.Point(10, 20)
+    $label.Size = New-Object System.Drawing.Size(280, 20)
+    $label.Text = 'Please enter the information in the space below:'
+    $form.Controls.Add($label)
+
+    # Add a text box for free text input
+    $textBox = New-Object System.Windows.Forms.TextBox
+    $textBox.Location = New-Object System.Drawing.Point(10, 40)
+    $textBox.Size = New-Object System.Drawing.Size(260, 20)
+    $form.Controls.Add($textBox)
+
+    # Add radio buttons for options
+    $radioButton1 = New-Object System.Windows.Forms.RadioButton
+    $radioButton1.Location = New-Object System.Drawing.Point(10, 70)
+    $radioButton1.Text = 'Option 1'
+    $form.Controls.Add($radioButton1)
+
+    $radioButton2 = New-Object System.Windows.Forms.RadioButton
+    $radioButton2.Location = New-Object System.Drawing.Point(10, 90)
+    $radioButton2.Text = 'Option 2'
+    $form.Controls.Add($radioButton2)
+
+    # Add a drop-down list (combo box)
+    $comboBox = New-Object System.Windows.Forms.ComboBox
+    $comboBox.Location = New-Object System.Drawing.Point(10, 120)
+    $comboBox.Items.AddRange('Item 1', 'Item 2', 'Item 3')
+    $form.Controls.Add($comboBox)
+
+    # Add a button to open a folder selection dialog
+    $folderButton = New-Object System.Windows.Forms.Button
+    $folderButton.Location = New-Object System.Drawing.Point(10, 150)
+    $folderButton.Text = 'Select Folder'
+    $folderButton.Add_Click({
+        $folderDialog = New-Object System.Windows.Forms.FolderBrowserDialog
+        if ($folderDialog.ShowDialog() -eq [System.Windows.Forms.DialogResult]::OK) {
+            $selectedFolder.Value = $folderDialog.SelectedPath
+        }
+    })
+    $form.Controls.Add($folderButton)
+
+    # Add a check box
+    $checkBox = New-Object System.Windows.Forms.CheckBox
+    $checkBox.Location = New-Object System.Drawing.Point(10, 180)
+    $checkBox.Text = 'Check this box'
+    $checkBox.Add_CheckedChanged({
+        $checkBoxState.Value = $checkBox.Checked
+    })
+    $form.Controls.Add($checkBox)
+
+    # Add an OK button
+    $okButton = New-Object System.Windows.Forms.Button
+    $okButton.Location = New-Object System.Drawing.Point(75, 220)
+    $okButton.Text = 'OK'
+    $okButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
+    $form.AcceptButton = $okButton
+    $form.Controls.Add($okButton)
+
+    # Add a Cancel button
+    $cancelButton = New-Object System.Windows.Forms.Button
+    $cancelButton.Location = New-Object System.Drawing.Point(150, 220)
+    $cancelButton.Text = 'Cancel'
+    $cancelButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
+    $form.CancelButton = $cancelButton
+    $form.Controls.Add($cancelButton)
 
     # Show the form
     $form.Topmost = $true
@@ -27,7 +104,5 @@ function Show-MyPowerShellForm {
 $myUserInput = $null
 $mySelectedOption = $null
 $mySelectedItem = $null
-
-Show-MyPowerShellForm ([ref]$myUserInput) ([ref]$mySelectedOption) ([ref]$mySelectedItem)
-
-# Now you can access $myUserInput, $mySelectedOption, and $mySelectedItem globally.
+$mySelectedFolder = $null
+$myCheckBoxState = $
